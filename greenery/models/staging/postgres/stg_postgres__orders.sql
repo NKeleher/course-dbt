8b33d72db06ea949a5e5b70aa212715e98ea3af9
Niall Keleher <niall@textio.com>
2023-04-15 23:13:42 +0000

    update orders staging model

---
+++ greenery/models/staging/postgres/stg_postgres__orders.sql
@@ -0,0 +1,21 @@
+{{
+  config(
+    materialized = 'view'
+  )
+}}
+
+SELECT
+  order_id
+  , user_id
+  , promo_id
+  , address_id
+  , created_at::timestamp AS created_at_utc
+  , order_cost
+  , shipping_cost
+  , order_total
+  , tracking_id
+  , shipping_service
+  , estimated_delivery_at::timestamp AS estimated_delivery_at
+  , delivered_at::timestamp AS delivered_at_utc
+  , 'status' AS order_status
+FROM {{ source('postgres', 'orders') }}
